02457460c2f374c5497f42b6ed106c5ab89abe01
davidgalindo-git <>
2025-11-27 16:48:22 +0100

    missing pages added

--- wesound/src/App.vue
+++ wesound/src/App.vue
@@ -1,30 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from "vue-router";
+import PlayerBar from "./components/PlayerBar.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-layout">
+
+    <!-- Navigation -->
+    <nav class="p-4 flex gap-4 border-b">
+      <router-link to="/">Home</router-link>
+      <router-link to="/feed">Feed</router-link>
+      <router-link to="/messages">Messages</router-link>
+      <router-link to="/profile/1">Profile</router-link>
+    </nav>
+
+    <!-- Page content -->
+    <main class="p-4">
+      <RouterView />
+    </main>
+
+    <!-- Global music player -->
+    <PlayerBar />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+nav a.router-link-active {
+  font-weight: bold;
 }
 </style>
