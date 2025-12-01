8bbeb1518cacd055bd7ea671647742c3c0cbc45d
davidgalindo-git <>
2025-12-01 20:24:45 +0100

    home auth disabled

--- wesound/src/router/index.js
+++ wesound/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: "/", name: "Home", component: () => import("../views/Home.vue"), meta: { requiresAuth: true } },
+  { path: "/", name: "Home", component: () => import("../views/Home.vue")},
   { path: '/login', name: 'Login', component: () => import("../views/LogIn.vue") },
   { path: "/feed", name: "Feed", component: () => import("../views/Feed.vue") },
   { path: "/messages", name: "Messages", component: () => import("../views/Messages.vue") },
