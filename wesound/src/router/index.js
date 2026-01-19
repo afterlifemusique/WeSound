05461c8af8d29d26b4759cf5e8bc5c9d0c3a7bd8
davidgalindo-git <>
2026-01-19 12:13:59 +0100

    develop init

--- wesound/src/router/index.js
+++ wesound/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-.
+
 const routes = [
   { path: "/", name: "Home", component: () => import("../views/Home.vue")},
   { path: '/login', name: 'Login', component: () => import("../views/LogIn.vue") },
